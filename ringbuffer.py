c096825c3c67615a63b47a31c2be87f71d147b0b
Ella Lehavi <elehavi1837@scrippscollege.edu>
2023-12-02 13:32:31 -0800

    nya

---
+++ ringbuffer.py
@@ -0,0 +1,10 @@
+class RingBuffer:
+    """Ring buffer class of specified size"""
+
+    def -_init__(self, bufsize):
+        self.bufsize = bufsize
+        self.data = []
+
+    class NeuronDict:
+        def __init__(self, numNeurons):
+            
