a007de28800bc370866d640b3c65a2975a54a984
Ella Lehavi <elehavi1837@scrippscollege.edu>
2023-12-04 15:07:34 -0800

    number of bins by bin size

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RingBuffoons
 # Hello this is Wolfgang Hutton
+
+##Functions we could add:
+Overwriting: Each bin stores list of dictionaries. Each dictionary represents the spike count of each neuron after one loop thru the buffer/theta phase
+ -- To do this, we need a helper function that creates a dict of neurons and theier spike counts at a given point.
--- ringbuffer.py
+++ ringbuffer.py
@@ -1,29 +1,39 @@
 import numpy as np
+import math
 
 class RingBuffer:
     """Ring buffer class of specified size w specified # of neurons"""
 
-    def __init__(self, bufSize, numNeurons):
-        self.bufSize = bufSize
-        self.numNeurons = numNeurons
-        self.data = [{x : 0 for x in range(numNeurons)} for x in range(bufSize)]
+    def __init__(self, numNeurons=3, binSize=math.pi/4):
+        self.binSize = binSize
+        self.numBins = calcNumBinsbyPhase(binSize, numNeurons) # each bin represents 2pi (one period of theta)
+        self.numNeurons = numNeurons #each neurons spike should take 3 periods
+        self.data = [{x : 0 for x in range(numNeurons)} for x in range(self.numBins)]
         self.oldest = 0
         self.curpos = 0
 
     def __repr__(self):
         output = ""
-        for i in range(self.bufSize):
-            if i == self.oldest % self.bufSize:
+        for i in range(self.numBins):
+            if i == 0:
+                binTitle = "PT"
+            else:
+                binTitle = str(self.binSize/(math.pi)*i) + "π"
+            output = output + "\n--"+ binTitle +"--\n"
+            if i == self.oldest % self.numBins:
                 output = output + "OLDEST\n"
             for j in self.data[i]:
                 output = output + "Neuron " + str(j) + ":"
                 for k in range(self.data[i][j]):
                     output = output + "|"
                 output += "\n"
-            if i == self.curpos % self.bufSize:
+            if i == self.curpos % self.numBins:
                 output = output + "\n CURRENT"
-            output = output + "\n-----\n"
+        output = output + "\n-----\n"
         return output
+
+
+
 
     def add(self, spikesPerNeuron):
         """ Takes in an ordered list that has
@@ -32,30 +42,28 @@ class RingBuffer:
             Increments bin """
         if self.curpos == self.oldest:
             self.oldest +=1
-        currBin = self.data[self.curpos % self.bufSize]
+        currBin = self.data[self.curpos % self.numBins]
         for i in range(self.numNeurons):
             currBin[i] = spikesPerNeuron[i]
         self.curpos += 1
+    
+    
 
-
-def calcBufSizebyPhase(data):
+def calcNumBinsbyPhase(binSize, numNeurons):
     """Calculates number of buffer slots
         based on # of theta phases"""
-        # TODO:
-        #neurons would shift pi/8 ahead of theta phase each time
-        #keep track of switches from bin to bin
-        #mulyiply by pi/8 to find where u r in rhythm
-        # track offset between neurons using theta cycle for bins
+    totalWave = 6*math.pi + 2*math.pi*(numNeurons-1)
+    return int(totalWave/binSize)
+
+def generateData(binSize, numNeurons):
+    # each neuron starts @ 2pi/binsize-1
+    # sample from gaussian
     pass
 
 #tests
-x = RingBuffer(5, 2)
-x.add([3, 0])
-x.add([2, 1])
-x.add([0,2])
-x.add([1,3])
-x.add([2,2])
-x.add([5,1])
+x = RingBuffer()
+x.add([3, 0, 0])
+x.add([2, 1, 0])
 print(repr(x))
 
 #TODO:generate data gaussianly
